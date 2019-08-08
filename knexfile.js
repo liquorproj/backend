fdd04c4aeffacebccb81bc2eee71c9e14a599913
Ang Xu <angxu1234@gmail.com>
2019-08-08 10:01:12 -0500

    initial installs

---
+++ knexfile.js
@@ -0,0 +1,63 @@
+// Update with your config settings.
+
+module.exports = {
+	development: {
+		client: "pg",
+		connection: process.env.DATABASE_URL || {
+			database: "liquor",
+			user: "user",
+			password: "pass"
+		},
+		pool: {
+			min: 2,
+			max: 10
+		},
+		migrations: {
+			directory: "./data/migrations",
+			tableName: "liquor"
+		},
+		seeds: {
+			directory: "./data/seeds"
+		}
+	},
+
+	testing: {
+		client: "pg",
+		connection: {
+			database: "test",
+			user: "user",
+			password: "pass"
+		},
+		pool: {
+			min: 2,
+			max: 10
+		},
+		migrations: {
+			directory: "./data/migrations",
+			tableName: "test"
+		},
+		seeds: {
+			directory: "./data/seeds"
+		}
+	},
+
+	production: {
+		client: "pg",
+		connection: process.env.DATABASE_URL || {
+			database: "liquor",
+			user: "user",
+			password: "pass"
+		},
+		pool: {
+			min: 2,
+			max: 10
+		},
+		migrations: {
+			directory: "./data/migrations",
+			tableName: "liquor"
+		},
+		seeds: {
+			directory: "./data/seeds"
+		}
+	}
+};
